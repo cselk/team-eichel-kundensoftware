225a7518e126a2970e14ca2171dc9ab8050d82bc
doenertrinker909 <jjj.io@web.de>
2026-02-10 10:58:00 +0100

    .

---
+++ Customer.py
@@ -0,0 +1,15 @@
+
+class Customer:
+    def __init__(self, first_name: str, last_name: str, id: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.id = id
+    
+    def get_first_name(self):
+        return self.first_name
+    
+    def get_last_name(self):
+        return self.last_name
+    
+    def is_before(self, c:Customer):
+        
