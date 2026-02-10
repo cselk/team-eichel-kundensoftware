ec3920f0d30d346c03dcf763ee18d5fc57cd0e8e
Christoph Selk <cselk@outlook.de>
2026-02-10 11:08:46 +0100

    update bintree.py

--- BinTree.py
+++ BinTree.py
@@ -1,11 +1,12 @@
 from treenode import TreeNode
 
+
 class BinTree:
-  def __init__(self):
-    self.root = TreeNode()
+    def __init__(self, data):
+        self.root = TreeNode(data)
 
-  def inser_sorted(self, dateElement):
-    self.root.insert_sorted(dataElement)
+    def inser_sorted(self, customer):
+        self.root.insert_sorted(customer)
 
-  def search_by_key(self, str):
-    return self.rott.search_by_key(str)
+    def search_by_key(self, str):
+        return self.root.search_by_key(str)
