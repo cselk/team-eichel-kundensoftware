6946e2cb563f0a35060f48d39dc73a296595af42
doenertrinker909 <jjj.io@web.de>
2026-02-10 11:20:59 +0100

    .

--- treenode.py
+++ treenode.py
@@ -8,8 +8,13 @@ class TreeNode:
     def get_data(self):
         return self.data
 
-    def insert_sorted(self, customer):
-        pass
+    def insert_sorted(self, c):
+        if data.is_before(c):
+            if left.insert_sorted(c):
+                left = TreeNode(c)
+        else:
+            if right.insert_sorted(c):
+                right = TreeNode(c)
 
     def search_by_key(self, key: str):
         if self.get_data().get_last_name() == key:
