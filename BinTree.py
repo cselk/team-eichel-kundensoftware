37948949fb8d9800ede684b39bd5e98da4d2c9ea
betterthananything <158348146+betterthananything@users.noreply.github.com>
2026-02-10 10:58:33 +0100

    Create BinTree.py

---
+++ BinTree.py
@@ -0,0 +1,9 @@
+class BinTree:
+  def __init__(self):
+    self.root = TreeNode()
+
+  def inser_sorted(self, dateElement):
+    self.root.insert_sorted(dataElement)
+
+  def search_by_key(self, str):
+    return self.rott.search_by_key(str)
