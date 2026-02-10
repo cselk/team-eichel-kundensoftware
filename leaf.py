3280f7e265615b10b4b5ef8f48a1a5c1527c4a7f
Christoph Selk <cselk@outlook.de>
2026-02-10 11:11:20 +0100

    Update leaf.py

--- leaf.py
+++ leaf.py
@@ -1,9 +1,9 @@
 class Leaf:
-    def __init__(self):
-        pass
     def get_data(self):
-        pass
-    def insert_sorted(self,Customer):
+        return None
+
+    def insert_sorted(self, Customer):
         return True
-    def search_by_key(self,str):
-        pass
+
+    def search_by_key(self, str):
+        return None
