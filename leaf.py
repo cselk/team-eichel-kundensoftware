f8f8c5ef945c33e20e5a0bb4849abf83a72a15ee
Dev_Jedi_Meister <65042910+Dev-Jedi-Meister@users.noreply.github.com>
2026-02-10 10:51:49 +0100

    Create leaf.py

---
+++ leaf.py
@@ -0,0 +1,10 @@
+class Leaf:
+    def __init__(self):
+        pass
+    def get_data(self):
+        pass
+    def insert_sorted(self,Customer):
+        return True
+    def search_by_key(self,str):
+        pass
+    
